67cb111a90ad4e799f3c9011a98e48951d13511a
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-05-16 00:05:18 -0400

    DONE

--- src/modify.js
+++ src/modify.js
@@ -1,26 +1,73 @@
 // These are your event handlers
-const clickCounterHandler = () => {
+// Question 1 
+const clickCounterHandler = (elements) => {
+  let num = Number(elements.target.dataset.clicks)
+num += 1 
+elements.target.dataset.clicks = num
+if (num === 0){
+  elements.target.textContent = `"I haven't been clicked!.`
+}else if(num === 1){
+  elements.target.textContent = `I've been clicked 1 time.`
+}else{
+  elements.target.textContent  = `I've been clicked ${num} times!`
+}
+}
+// Question 2 
+const handleKeydown = (event) => {
+  let pTagUpdate = document.getElementById("keydown-tracker")
+  pTagUpdate.textContent = `You pressed ${event.code}`
 };
-
-const handleKeydown = () => {
-};
-
+// Question 3
+const clickCounterHandler3 = (elements) => {
+  let num = Number(elements.target.dataset.clicks)
+num += 1 
+elements.target.dataset.clicks = num
+if (num === 0){
+  elements.target.textContent = `"I shouldn't be inline`
+}else if(num === 1){
+  elements.target.textContent = `I've been clicked 1 time.`
+}else{
+  elements.target.textContent  = `I've been clicked ${num} times!`
+}
+}
+//Question 4
 // We've started this one for you
-const handleDelegation = (event) => {
-  const resultSpan = document.querySelector('#delegation-result');
-  resultSpan.textContent = event.target.textContent;
+const handleDelegation = (events) => {
+    const resultSpan = document.querySelector('#delegation-result');
+    if (events.target.tagName === "BUTTON") {
+      resultSpan.textContent = events.target.textContent;
+    }
 };
-
+// Question 5
 const addNewRandomNumber = () => {
-};
+let addNumber = document.querySelector('#random-numbers');
+let newNumber = document.createElement('li');
 
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+newNumber.textContent = randomNumber;
+addNumber.appendChild(newNumber);
+}
 
 // Select the elements and attach your event handlers inside main
 const main = () => {
-  const delegationContainer = document.querySelector('#delegation');
-  delegationContainer.addEventListener('click', handleDelegation);
-
-  
+  const click = document.querySelector("#click-button")
+  click.addEventListener('click', clickCounterHandler)
+  // Question 2
+  document.body.addEventListener('keydown', handleKeydown);
+  //Question 3
+const inlineButton = document.getElementById("inline-example");
+inlineButton.addEventListener("click", clickCounterHandler3);
+// Question 4
+const delegationContainer = document.querySelector('#delegation');
+delegationContainer.addEventListener('click', handleDelegation);
+//Question 5
+const addNum = document.getElementById("add-random-num")
+addNum.addEventListener("click", addNewRandomNumber)
+// Question 6
+const removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', () => {
+  delegationContainer.removeEventListener('click', handleDelegation);
+});
 };
 
 main();
